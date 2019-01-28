3691e18ef4ad1a96103768df1be2fa9428ab8448
nakraniprakash <mr.prakashnakrani@gmail.com>
2019-01-28 19:23:12 +0530

    Deleting old structure

--- src/Store/actions/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const SIGNUP_START = 'SIGNUP_START';
-export const SIGNUP_SUCCESS  = 'SIGNUP_SUCCESS';
-export const SIGNUP_FAIL = 'SIGNUP_FAIL';
-export const SIGNUP_FINISH = 'SIGNUP_FINISH';
--- src/Store/actions/index.js
+++
@@ -1,3 +0,0 @@
-export {
-    signup
-} from './signup';
